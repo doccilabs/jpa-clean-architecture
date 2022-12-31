7085670bb943a8a6d4bbeed7ea5811cda44372f3
BrianDYKim <18k7102dy@naver.com>
2023-01-01 00:35:37 +0900

    add : port layer 추가

---
+++ adapter/dao/src/main/kotlin/team/me/dao/DaoApplication.kt
@@ -0,0 +1,11 @@
+package team.me.dao
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DaoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DaoApplication>(*args)
+}
---
+++ adapter/dao/src/test/kotlin/team/me/dao/DaoApplicationTests.kt
@@ -0,0 +1,13 @@
+package team.me.dao
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DaoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
